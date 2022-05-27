98a1f1bd04cddc2ee2fbd94b0bc26d7fa1188661
Lauan Amorim <62584060+LauanAmorim@users.noreply.github.com>
2022-05-27 12:05:37 -0300

    Add files via upload

---
+++ projetoClasse/projetoClasse/ClassePessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace projetoClasse
+{
+    internal class ClassePessoa
+    {
+
+        private string nome;
+        private int idade;
+        private double altura;
+
+        public ClassePessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+            this.altura = 0;
+        }
+
+        public string Nome {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Idade {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public double Altura { 
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public override string ToString()
+        {
+            return (String.Format($"Nome: {nome} \n Idade: {idade} \n Altura: {altura}"));
+        }
+    }
+}
---
+++ projetoClasse/projetoClasse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.Write("Digite o nome da pessoa: "); string n = Console.ReadLine();
+            Console.Write($"Digite a Idade de {n}: "); int i = int.Parse(Console.ReadLine());
+            Console.Write($"Digite a altura de {n}: "); double a = double.Parse(Console.ReadLine());
+            ClassePessoa pessoa = new ClassePessoa();
+         
+
+            Console.WriteLine("\n \n***Dados da pessoa***");
+            Console.WriteLine($"Nome: {n} \n Idade: {i} \n Altura: {a}");
+
+            Console.WriteLine(pessoa.Altura.ToString());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
